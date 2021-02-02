5fc9f688f29f60035a4578dcc18ac459a9b96cd3
unknown <dariadruzh5@gmail.com>
2021-02-02 20:46:19 +0300

    Исправление замечания. Добавление throw, разделение ошибок

--- js/main.js
+++ js/main.js
@@ -1,23 +1,36 @@
 // Источник: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 
 function getRandomIntegerNumber(min, max) {
-  if (min > max || max < 0 || min < 0) {
-    alert('Неверно введён диапазон');
-    return null;
+  if (min > max) {
+    throw new Error('Начальное значение диапазона больше конечного');
+  }
+
+  if (max < 0) {
+    throw new Error('Конечное значение диапазона не может являться отрицательным числом');
+  }
+
+  if (min < 0) {
+    throw new Error('Начальное значение диапазона не может являться отрицательным числом');
   }
 
   return Math.floor((Math.random() * (max - min + 1)) + min);
 }
 
 function getRandomFloatNumber(min, max, decimalPlaces) {
-  if (min > max || max < 0 || min < 0) {
-    alert('Неверно введён диапазон');
-    return null;
+  if (min > max) {
+    throw new Error('Начальное значение диапазона больше конечного');
+  }
+
+  if (max < 0) {
+    throw new Error('Конечное значение диапазона не может являться отрицательным числом');
+  }
+
+  if (min < 0) {
+    throw new Error('Начальное значение диапазона не может являться отрицательным числом');
   }
 
   if (decimalPlaces < 0 || decimalPlaces > 20) {
-    alert('Неверно введено количество знаков после запятой');
-    return null;
+    throw new Error('Неверно введено количество знаков после запятой');
   }
 
   return (Math.random() * (max - min) + min).toFixed(decimalPlaces);
