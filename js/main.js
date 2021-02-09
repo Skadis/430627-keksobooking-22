aba9a97bc58d4bb47b5a833e6f6b519ce60fc5b6
unknown <dariadruzh5@gmail.com>
2021-02-09 19:03:23 +0300

    Исправление замечаний, добавление функции для генерации пути к фото

--- js/main.js
+++ js/main.js
@@ -1,8 +1,6 @@
-// Источник: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random, https://learn.javascript.ru/
 const TYPES = ['palace', 'flat', 'house', 'bungalow'];
 const TIME = ['12:00', '13:00', '14:00'];
 const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-const PHOTOS_URL = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg',  'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
 const QUANTITY_OF_OFFERS = 10;
 
 function getRandomIntegerNumber(min, max) {
@@ -59,43 +57,45 @@ const getRandomArrayElement = (elements) => {
   return elements[getRandomIntegerNumber(0, elements.length - 1)];
 }
 
-const createAuthor = () => {
+const generatePhotoUrl = (firstNumber, lastNumber) =>  {
+  let result = [];
 
-  return {
-    avatar: `img/avatars/user0${getRandomIntegerNumber(1, 8)}.png`,
+  for (let i = firstNumber; i <= lastNumber; i++) {
+    result.push(`http://o0.github.io/assets/images/tokyo/hotel${i}.jpg`);
   }
+
+  return result;
 }
 
 const createOffer = () => {
 
   return {
-    title: 'Тут будет заголовок объявления',
-    address: `${getRandomFloatNumber(35.65000, 35.70000, 5)}, ${getRandomFloatNumber(139.70000, 139.80000, 5)}`,
-    price: getRandomIntegerNumber(0, 1000000),
-    type: getRandomArrayElement(TYPES),
-    rooms: getRandomIntegerNumber(1, 30),
-    guests: getRandomIntegerNumber(1, 100),
-    checkin: getRandomArrayElement(TIME),
-    checkout: getRandomArrayElement(TIME),
-    features: createUniqueArray(FEATURES),
-    description: 'Тут будет описание объявления',
-    photos: createUniqueArray(PHOTOS_URL),
-    location: {
-      x: getRandomFloatNumber(35.65000, 35.70000, 5),
-      y: getRandomFloatNumber(139.70000, 139.80000, 5),
+    author: {
+      avatar: `img/avatars/user0${getRandomIntegerNumber(1, 8)}.png`,
+    },
+
+    offer: {
+      title: 'Тут будет заголовок объявления',
+      address: `${getRandomFloatNumber(35.65000, 35.70000, 5)}, ${getRandomFloatNumber(139.70000, 139.80000, 5)}`,
+      price: getRandomIntegerNumber(0, 1000000),
+      type: getRandomArrayElement(TYPES),
+      rooms: getRandomIntegerNumber(1, 30),
+      guests: getRandomIntegerNumber(1, 100),
+      checkin: getRandomArrayElement(TIME),
+      checkout: getRandomArrayElement(TIME),
+      features: createUniqueArray(FEATURES),
+      description: 'Тут будет описание объявления',
+      photos: createUniqueArray(generatePhotoUrl(1, 3)),
+      location: {
+        x: getRandomFloatNumber(35.65000, 35.70000, 5),
+        y: getRandomFloatNumber(139.70000, 139.80000, 5),
+      },
     },
   }
 }
 
-const createNewNearOffer = () => {
-  const OFFERS = createOffer();
-  const AUTHORS = createAuthor();
-
-  return Object.assign({}, AUTHORS, OFFERS);
-}
-
 const createManyNearOffers = (quantityOfOffers) => {
-  let nearOffers = new Array (quantityOfOffers).fill(null).map(() => createNewNearOffer());
+  const nearOffers = new Array (quantityOfOffers).fill(null).map(() => createOffer());
 
   return nearOffers;
 }
